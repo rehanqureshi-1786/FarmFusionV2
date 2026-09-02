f301f55e9f0996b4da341a3794a7c0026bb4ea56
rehanqureshi-1786 <rafikqureshi0971@gmail.com>
2026-09-02 21:32:26 +0530

    fix(network): update ApiConfig.BASE_URL with active Wi-Fi IP

--- frontend/app/src/main/java/com/example/farmfusionapp/network/ApiConfig.kt
+++ frontend/app/src/main/java/com/example/farmfusionapp/network/ApiConfig.kt
@@ -1,8 +1,8 @@
 package com.example.farmfusionapp.network
 
 object ApiConfig {
-    // Current Laptop Wi-Fi Local Network IP:
-    const val BASE_URL = "http://10.63.73.222:8000/"
+    // Current Laptop Wi-Fi Local Network IP (interface wlp2s0):
+    const val BASE_URL = "http://10.188.230.222:8000/"
 
     // If connected via USB with 'adb reverse tcp:8000 tcp:8000', you can also use:
     // const val BASE_URL = "http://127.0.0.1:8000/"
